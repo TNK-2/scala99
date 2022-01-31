9215db3efcf09289e604ea9ee3e6a7e8147ed166
Yu-s <yuuu1993g@gmail.com>
2022-01-31 14:09:03 +0900

    p01: get last element

--- src/main/scala/list/ListOperationSample.scala
+++ src/main/scala/list/ListOperationSample.scala
@@ -1,5 +1,18 @@
 package list
 
-class ListOperationSample {
+object ListOperationSample {
 
+  /**
+   * P01
+   */
+  def findLastElement[A](list: List[A]): A = list.last
+
+  /**
+   * P01
+   */
+  def findLastElementRecursive[A](list: List[A]): A = list match {
+    case h :: Nil => h
+    case _ :: tail => findLastElementRecursive(tail)
+    case _ => throw new NoSuchElementException
+  }
 }
--- src/main/scala/list/Main.scala
+++ src/main/scala/list/Main.scala
@@ -3,6 +3,13 @@ package list
 object Main {
 
   def main (args: Array[String]): Unit = {
-    println("hello world")
+
+    val lastElement = ListOperationSample
+      .findLastElement(List(2, 3, 5, 1))
+    val lastElementRecursive = ListOperationSample
+      .findLastElementRecursive(List(2, 3, 5, 1))
+
+    println("P01 :: The last Element is : " + lastElement)
+    println("PO1 :: The last Element Recursive is : " + lastElementRecursive)
   }
 }
